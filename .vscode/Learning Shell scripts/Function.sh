67ffdacc67c3c8174e85a6293962dcfec537dab5
Deepshri78 <srivastavadeepak0311@gmail.com>
2023-06-16 15:25:18 -0400

    Correction 8

--- .vscode/Learning Shell scripts/Function.sh
+++ .vscode/Learning Shell scripts/Function.sh
@@ -1,16 +1,17 @@
 declare -i a
 declare -i b
 declare -i c
-read -p "Enter first number: " a
-read -p "Enter second number: " b
 
 function SWAP ()
 {
     $var=$(($1+$2))
-    $1=$(($var-$1))
-    $2=$(($var-$2))
+    a=$(($var-$1))
+    b=$(($var-$2))
 
 }
+
+read -p "Enter first number: " a
+read -p "Enter second number: " b
 
 echo "Value of a is " $a
 echo "Value of b is " $b
