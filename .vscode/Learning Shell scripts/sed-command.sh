b47b13da8870c0a48b846a4a6f9dac0b36125300
Deepshri78 <srivastavadeepak0311@gmail.com>
2023-06-16 16:40:36 -0400

    Correction1

--- .vscode/Learning Shell scripts/sed-command.sh
+++ .vscode/Learning Shell scripts/sed-command.sh
@@ -1,3 +1,3 @@
-sed 's/linux/unix/' sample.txt
-sed 's/linux/unix/2' sample.txt
-sed 's/linux/unix/g' sample.txt
+sed 's/linux/unix/' Sample.txt
+sed 's/linux/unix/2' Sample.txt
+sed 's/linux/unix/g' Sample.txt
