80296212b276d096ff3738c0a271840df707b003
Deepshri78 <srivastavadeepak0311@gmail.com>
2023-06-16 11:49:29 -0400

    More colors

--- .vscode/Learning Shell scripts/Print-Bash.sh
+++ .vscode/Learning Shell scripts/Print-Bash.sh
@@ -1,4 +1,4 @@
 #!
-echo " This is to practice print command in scripts."
+echo -e "\e[31m This is to practice print command in scripts. \e[0m]"
 
 echo -e "\e[32m This is to color code your print. \e[0m"
