d4ad78dcf46051f4d8c7391617940abceb4b5cd7
Deepshri78 <srivastavadeepak0311@gmail.com>
2023-06-16 15:13:06 -0400

    Functions

---
+++ .vscode/Learning Shell scripts/Function.sh
@@ -0,0 +1,21 @@
+declare -i a
+declare -i b
+declare -i c
+read -p "Enter first number: " a
+read -p "Enter second number: " b
+
+SWAP ()
+{
+    c = ($1+$2)
+    a = $c-$1
+    b = $c-$2
+
+}
+read -p "Enter first number: " a
+read -p "Enter second number: " b
+echo "Value of a is " $a
+echo "Value of b is " $b
+SWAP (a,b)
+
+echo "Value of a is " $a
+echo "Value of b is " $b
