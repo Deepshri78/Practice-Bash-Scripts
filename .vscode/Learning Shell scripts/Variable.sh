84d0115156a44962f8672b9ab92c8eb6ab16aa3e
Deepshri78 <srivastavadeepak0311@gmail.com>
2023-06-16 11:56:59 -0400

    Correction 1

--- .vscode/Learning Shell scripts/Variable.sh
+++ .vscode/Learning Shell scripts/Variable.sh
@@ -3,7 +3,7 @@ var2 = 'abc'
 var3 = abc
 var4 = true
 
-echo 'value of var1 = ' $var1
-echo 'value of var2 = ' $var2
-echo 'value of var3 = ' $var3
-echo 'value of var4 = ' $var4
+echo 'value of var1 =  $var1'
+echo 'value of var2 =  $var2'
+echo 'value of var3 =  $var3'
+echo 'value of var4 =  $var4'
