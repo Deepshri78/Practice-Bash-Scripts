97c4e7604a610b80c8f360019509c7ea5d61ec6b
Deepshri78 <srivastavadeepak0311@gmail.com>
2023-06-16 14:51:45 -0400

    Correction 1

--- .vscode/Learning Shell scripts/If-Statement.sh
+++ .vscode/Learning Shell scripts/If-Statement.sh
@@ -1,9 +1,9 @@
-Declare -i a
+declare -i a
 a=3
 
-if ($a -gt 10)
+if [$a -gt 10]
 echo " a is greater than 10"
-elif ($a -lt 10)
+elif [$a -lt 10]
 echo " a is less than 10"
 else
 echo " a is equal to 10"
